17cc6d9c449941158f5bdec2febf6aa454b47a40
diggedypomme <superpomme@gmail.com>
2023-04-23 13:38:40 +0100

    Update new3.py

--- running_code/new3.py
+++ running_code/new3.py
@@ -63,13 +63,16 @@ def send_values2(sebd):
 
     #testecho= '''echo "CMD7234 V005P1533V004P1383V003P1692V017P1515V016P1430V015P1768V011P1424V010P1413V009P1713V014P1544V013P1359V012P1670V002P1515V001P1450V000P1463V008P1398V007P1448V006P1737T0150##\r" > /dev/ttymxc2'''
     
-    testecho= '''echo "CMD7234 {}T0150##\r" > /dev/ttymxc2'''.format(sebd)
+    testecho= '''echo "CMD7234 {}##\r" > /dev/ttymxc2'''.format(sebd)
   
     os.system(testecho)
     return(sebd)
 
 
-
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 if __name__ == '__main__':
